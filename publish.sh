0418a4e894c5cbd636f7c9caf4ff83dd93699e18
张玉川 <zhangyc121@chinatelecom.cn>
2023-12-28 15:01:49 +0800

    Update publish script, integrate init.sh into it.

--- publish.sh
+++ publish.sh
@@ -1,8 +1,25 @@
-#!/bin/bash
+#! /bin/bash
+
+echo "============ Pull rpm packages ============"
+cd rpms/
+./init.sh
+cd -
+
+echo "============ Pull x86_64 docker images, and save to tar ============"
+cd tar-x86_64/
+./init.sh
+cd -
+
+echo "============ Pull aarch64 docker images, and save to tar ============"
+cd tar-aarch64/
+./init.sh
+cd -
+
 dist=("ctl2" "ctl3")
 arch=("x86_64" "aarch64")
 
-#Generate offline tar balls and upload onto repo server
+# Generate offline tar balls and upload onto repo server
+echo "============ Generate offline tar balls and upload onto repo server ============"
 rm -rf /tmp/k8s-install-offline
 mkdir -p /tmp/k8s-install-offline/rpms
 cp -r config k8s-install-offline README.md /tmp/k8s-install-offline
@@ -18,13 +35,14 @@ for a in ${arch[@]};do
         cd -
         rsync --delete --partial -avzuP /tmp/k8sinstall-$d-$a.tgz ctyunos@124.236.120.248::ctyunos/ctyunos_testing/k8sinstall-$d-$a.tgz --password-file=rsync.password
         echo "/tmp/k8sinstall-$d-$a.tgz is published!"
-        #rm -rf /tmp/k8sinstall-*.tgz
+        # rm -rf /tmp/k8sinstall-*.tgz
     done
     rm -rf /tmp/k8s-install-offline/tar-$a
 done
 rm -rf /tmp/k8s-install-offline
 
-#Retar source code and cherrypick to build branch
+# Retar source code and cherrypick to build branch
+echo "============ Retar source code and cherrypick to build branch ============"
 git status | grep "nothing to commit"
 [[ $? == 0 ]] || (echo "Some git changes still not committed. exit.." && exit)
 rm -rf /tmp/k8s-install
@@ -35,7 +53,7 @@ tar zcf k8s-install.tgz k8s-install/
 cd -
 git checkout obsbuild
 mv /tmp/k8s-install.tgz .
-rpmbuild -bb --define "_sourcedir $(pwd)" *.spec #Just a test
+rpmbuild -bb --define "_sourcedir $(pwd)" *.spec  # Just a test
 git add .
-git commit -m "commit changes into gitlab"
+git commit -m "commit changes into obsbuild branch."
 git push
