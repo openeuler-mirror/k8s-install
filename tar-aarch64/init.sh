3918e5024f21f2eaa2cb3b1ab75ddd94cf42ad7a
张玉川 <zhangyc121@chinatelecom.cn>
2023-12-28 14:27:24 +0800

    Adjust and optimize the process of pulling Docker images.

--- tar-aarch64/init.sh
+++ tar-aarch64/init.sh
@@ -1,4 +1,19 @@
-#!/bin/bash
+#! /bin/bash
+
+rm -f *.tar
+
+# pull docker images.
+docker pull docker.ctyun.cn:60001/base-aarch64/flannel:v0.21.0
+docker pull docker.ctyun.cn:60001/base-aarch64/flannel-cni-plugin:v1.1.2
+docker pull docker.ctyun.cn:60001/base-aarch64/kube-proxy:v1.20.2
+docker pull docker.ctyun.cn:60001/base-aarch64/kube-apiserver:v1.20.2
+docker pull docker.ctyun.cn:60001/base-aarch64/kube-controller-manager:v1.20.2
+docker pull docker.ctyun.cn:60001/base-aarch64/kube-scheduler:v1.20.2
+docker pull docker.ctyun.cn:60001/base-aarch64/etcd:3.4.13-0
+docker pull docker.ctyun.cn:60001/base-aarch64/coredns:1.7.0
+docker pull docker.ctyun.cn:60001/base-aarch64/pause:3.2
+
+# save docker images to tar.
 docker save docker.ctyun.cn:60001/base-aarch64/flannel:v0.21.0  -o flannel.tar
 docker save docker.ctyun.cn:60001/base-aarch64/flannel-cni-plugin:v1.1.2 -o flannel-cni-plugin.tar
 docker save docker.ctyun.cn:60001/base-aarch64/kube-proxy:v1.20.2  -o kube-proxy.tar
